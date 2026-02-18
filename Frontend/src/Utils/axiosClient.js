8c8bc8fcea82f9ea06106980b0eabb4da11e1b14
Priyanshu Sharma <priyanshusharma9998@gmail.com>
2026-02-18 21:49:47 +0530

    Update axios baseURL to production server

--- Frontend/src/Utils/axiosClient.js
+++ Frontend/src/Utils/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'http://localhost:5005',
+    // baseURL: 'http://localhost:5005',
+    baseURL:'https://coding-platform-4.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
