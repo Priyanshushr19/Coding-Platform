29f645946b42c2ec018d52873518d3267c713112
Priyanshu Sharma <priyanshusharma9998@gmail.com>
2026-02-18 21:43:23 +0530

    Implement MongoDB connection function
    Add a function to connect to MongoDB and handle connection events.

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,1 +1,10 @@
 import mongoose from "mongoose";
+
+export const connectDB=async()=>{
+    try {
+        mongoose.connection.on('connected',()=>console.log("DB connection done "))
+        await mongoose.connect(process.env.MONGO_URI)
+    } catch (error) {
+        console.log(error.message);
+    }
+}
