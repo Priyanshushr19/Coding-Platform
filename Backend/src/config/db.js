1c6d07c29aff25d225ea240582a8bfc0750a5291
Priyanshu Sharma <priyanshusharma9998@gmail.com>
2026-03-03 21:05:55 +0530

    Update db.js

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   const uri = process.env.MONGO_URI;
+  console.log(uri);
 
   if (!uri) {
     throw new Error("MONGO_URI missing");
