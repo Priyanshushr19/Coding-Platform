2b1f14be176987a0368364b1eea86892f1f88938
Priyanshu Sharma <priyanshusharma9998@gmail.com>
2026-03-03 21:49:00 +0530

    Update userAth.js

--- Backend/src/controllers/userAth.js
+++ Backend/src/controllers/userAth.js
@@ -142,12 +142,18 @@ const login = async (req, res) => {
       role: user.role,
     };
 
-    const token = jwt.sign(
-      { _id: user._id, emailId: emailId, role: user.role },
-      process.env.JWT_KEY,
-      { expiresIn: "1h" }
-    );
-
+    // const token = jwt.sign(
+    //   { _id: user._id, emailId: emailId, role: user.role },
+    //   process.env.JWT_KEY,
+    //   { expiresIn: "1h" }
+    // );
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,        // production me required
+      sameSite: "none",    // cross-domain ke liye required
+      maxAge: 60 * 60 * 1000
+    });
+      
     res.cookie("token", token, {
       httpOnly: true,
       sameSite: "lax",
